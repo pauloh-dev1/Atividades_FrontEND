ce03b9f923d856582eeca6bdd42d9c972afa6b50
Paulo Silva <paulohenriee@gmail.com>
2026-05-06 00:19:19 -0300

    Add files via upload

---
+++ Atividades 2º Bimestre/Prova - dia 3/script.js
@@ -0,0 +1,13 @@
+function mostrarImagem() {
+
+    let div = document.getElementById("resultado");
+
+    if (div.innerHTML === "") {
+        let img = document.createElement("img");
+
+        img.src = "img/Tabela_Jogos.png"; 
+        img.alt = "Tabela de Jogos";
+
+        div.appendChild(img);
+    }
+}
